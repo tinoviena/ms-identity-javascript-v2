7fb3769fad680eb8d1b235b58913a3c186245efd
MKU <tino.viena@yahoo.com>
2022-12-29 15:27:59 +0000

    works

--- server.js
+++ server.js
@@ -16,11 +16,19 @@ app.use(morgan('dev'));
 // Setup app folders.
 app.use(express.static('app'));
 
+app.get('/auth/redirect', (req, res) => {
+    redirPagePath = "/workspaces/ms-identity-javascript-v2/app/redirected.html"; //__dirname + '/app/redirected.html';
+    console.log(redirPagePath)
+//    res.sendFile(path.join(redirPagePath));
+    res.sendFile(redirPagePath);
+});
+
 // Set up a route for index.html
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
+    console.log("__dirname=${__dirname}")
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Start the server.
 app.listen(port);
-console.log(`Listening on port ${port}...`);
+console.log(`Listening on port ${port}...${__dirname}`);
